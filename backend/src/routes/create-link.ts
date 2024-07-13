937877a269a34a909b69696f06a6e6c6b069374a
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-07-13 18:43:56 -0300

    feat: add link creation route on trip

---
+++ backend/src/routes/create-link.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod"; // validation library
+import { prisma } from "../lib/prisma";
+
+export async function createLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/links",
+    {
+      // schema is the validation for the request body by ZodTypeProvider
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(), // check if the tripId is a valid uuid
+        }),
+        body: z.object({
+          title: z.string().min(4),
+          url: z.string().url(), // check if the url is a valid url
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params; // get the tripId from the request params
+      const { title, url } = request.body; // get information from the request body done by the validation
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      const link = await prisma.link.create({
+        data: {
+          title,
+          url,
+          trip_id: tripId,
+        },
+      });
+
+      return { linkId: link.id };
+    }
+  );
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,7 @@ import { createTrip } from "./routes/create-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
 
 const app = fastify();
 
@@ -24,6 +25,7 @@ app.register(confirmTrip); // Register the confirmTrip route
 app.register(confirmParticipants); // Register the confirmParticipants route
 app.register(createActivity); // Register the createActivity route
 app.register(getActivities); // Register the getActivities route
+app.register(createLink); // Register the createLink route
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
