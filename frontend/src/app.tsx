eda9cc4a6fa6167043b968b1574dc6931d6fbcc8
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-07-14 16:37:30 -0300

    feat: add travel guest modal on homepage

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,11 +4,19 @@ import {
   ArrowRight,
   UserRoundPlus,
   Settings2,
+  X,
+  AtSign,
+  Plus,
 } from "lucide-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    "ghabrielmolina@hotmail.com",
+    "ghabriel.m@edu.pucrs.br",
+  ]);
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true);
@@ -16,6 +24,38 @@ export function App() {
 
   function closeGuestsInput() {
     setIsGuestsInputOpen(false);
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    // The event is of type FormEvent, and the target is of type HTMLFormElement (generic)
+    event.preventDefault(); // Prevents the page from reloading when submitting the form (default reload behavior)
+
+    const data = new FormData(event.currentTarget); // To get event data from the form, you need to say that it is happening from an HTMLFormElement specifically
+    const email = data.get("email")?.toString(); // Get the value of the email field and convert it to a string
+
+    if (!email) return; // If the email is empty, return
+
+    if (emailsToInvite.includes(email)) return; // If the email is already in the list, return
+
+    setEmailsToInvite([...emailsToInvite, email]); // Spread the current emails and add the new email
+
+    event.currentTarget.reset(); // Reset the form after adding the email
+  }
+
+  function removeEmailFromInvites(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove
+    );
+
+    setEmailsToInvite(newEmailList);
   }
 
   return (
@@ -73,14 +113,16 @@ export function App() {
           {isGuestsInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
               {/* flex-1 Try to occupy as much space as possible within the div */}
-              <div className="flex items-center gap-2 flex-1">
+              <button
+                type="button"
+                onClick={openGuestsModal}
+                className="flex items-center gap-2 flex-1 text-left"
+              >
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  placeholder="Quem estará na viagem?"
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
-              </div>
+                <span className="text-zinc-400 text-lg flex-1">
+                  Quem estará na viagem?
+                </span>
+              </button>
               <div className="w-px h-6 bg-zinc-800" /> {/* Vertical line */}
               <button className="bg-lime-300 text-lime-950 rounded-lg font-medium px-5 py-2 flex items-center gap-2 hover:bg-lime-400">
                 Confirmar viagem
@@ -103,6 +145,67 @@ export function App() {
           .
         </p>
       </div>
+
+      {isGuestsModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold text-lg">Selecionar convidados</h2>
+                <button type="button" onClick={closeGuestsModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-zinc-400 text-sm">
+                Os convidados irão receber e-mails para confirmar a participação
+                na viagem.
+              </p>
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {emailsToInvite.map((email) => {
+                return (
+                  // When using react properties such as map, you need to pass a unique identification key to each element
+                  <div
+                    key={email}
+                    className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
+                  >
+                    <span className="text-zinc-300">{email}</span>
+                    <button type="button">
+                      <X
+                        onClick={() => removeEmailFromInvites(email)}
+                        className="size-4 text-zinc-400"
+                      />
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="w-full h-px bg-zinc-800" /> {/* Horizontal line */}
+            <form
+              onSubmit={addNewEmailToInvite}
+              className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2"
+            >
+              <div className="flex items-center gap-2 flex-1 px-2">
+                <AtSign className="text-zinc-400 size-5" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Digite o e-mail do convidado"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-950 rounded-lg font-medium px-5 py-2 flex items-center gap-2 hover:bg-lime-400"
+              >
+                Confirmar viagem
+                <Plus className="size-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
