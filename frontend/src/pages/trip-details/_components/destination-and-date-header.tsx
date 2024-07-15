f54fe21fd5021bd5df545d43ab3c6015199289da
Ghabriel Molina <ghabrielmolina@hotmail.com>
2024-07-14 23:05:43 -0300

    feat: add db information to the trip details page screen

--- frontend/src/pages/trip-details/_components/destination-and-date-header.tsx
+++ frontend/src/pages/trip-details/_components/destination-and-date-header.tsx
@@ -1,18 +1,44 @@
-import { MapPin, Calendar, Settings2 } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button } from "../../../_components/button";
+import { api } from "../../../lib/axios";
+
+interface Trip {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
+}
 
 export function DestinationAndDateHeader() {
+  const { tripId } = useParams(); // returns parameters that exist in the URL
+  const [trip, setTrip] = useState<Trip | undefined>();
+
+  // useEffect controls when something will execute upon state change [''], so as not to render repeatedly
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip)); // Request to the backend and set the trip state with interface Trip data type
+  }, [tripId]);
+
+  const displayedDate = trip
+    ? format(trip.starts_at, "d'  de  'LLL")
+        .concat(" até ")
+        .concat(format(trip.ends_at, "d'  de  'LLL"))
+    : null;
+
   return (
     <div className="h-16 px-4 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex gap-2 items-center">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-zinc-100">Florianópolis, Brasil</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </div>
 
       <div className="flex items-center gap-5">
         <div className="flex gap-2 items-center">
           <Calendar className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">16 a 14 de Agosto</span>
+          <span className="text-zinc-100">{displayedDate}</span>
         </div>
         <div className="w-px h-6 bg-zinc-800" /> {/* Vertical line */}
         <Button variant="secondary">
--- frontend/src/pages/trip-details/_components/guests.tsx
+++ frontend/src/pages/trip-details/_components/guests.tsx
@@ -1,68 +1,58 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button } from "../../../_components/button";
+import { api } from "../../../lib/axios";
+
+interface Participant {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
+}
 
 export function Guests() {
+  const { tripId } = useParams(); // returns parameters that exist in the URL
+  const [participants, setParticipants] = useState<Participant[]>();
+
+  // useEffect controls when something will execute upon state change [''], so as not to render repeatedly
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/participants`)
+      .then((response) => setParticipants(response.data.participants)); // Request to the backend and set the trip state with interface Participants data type
+  }, [tripId]);
+
   return (
     <div>
       <div className="space-y-6">
         <h2 className="font-semibold text-xl">Convidados</h2>
 
         <div className="space-y-5">
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="font-medium text-zinc-100 block">
-                Jessica White
-              </span>
-              <span className="font-sm text-zinc-400 block truncat">
-                {/*truncate is a tailwind class that ... in text*/}
-                jessica.white44@yahoo.com
-              </span>
-            </div>
-            <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-            {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
-          </div>
-
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="font-medium text-zinc-100 block">
-                Jessica White
-              </span>
-              <span className="font-sm text-zinc-400 block truncat">
-                {/*truncate is a tailwind class that ... in text*/}
-                jessica.white44@yahoo.com
-              </span>
-            </div>
-            <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-            {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
-          </div>
-
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="font-medium text-zinc-100 block">
-                Jessica White
-              </span>
-              <span className="font-sm text-zinc-400 block truncat">
-                {/*truncate is a tailwind class that ... in text*/}
-                jessica.white44@yahoo.com
-              </span>
-            </div>
-            <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-            {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
-          </div>
-
-          <div className="flex items-center justify-between gap-4">
-            <div className="space-y-1.5">
-              <span className="font-medium text-zinc-100 block">
-                Jessica White
-              </span>
-              <span className="font-sm text-zinc-400 block truncat">
-                {/*truncate is a tailwind class that ... in text*/}
-                jessica.white44@yahoo.com
-              </span>
-            </div>
-            <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-            {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
-          </div>
+          {participants?.map((participant, index) => {
+            return (
+              <div
+                key={participant.id}
+                className="flex items-center justify-between gap-4"
+              >
+                <div className="space-y-1.5">
+                  <span className="font-medium text-zinc-100 block">
+                    {participant.name ?? "Convidado " + index}
+                  </span>
+                  <span className="font-sm text-zinc-400 block truncat">
+                    {/*truncate is a tailwind class that ... in text*/}
+                    {participant.email}
+                  </span>
+                </div>
+                {participant.is_confirmed ? (
+                  <CheckCircle2 className="size-5 text-green-400 shrink-0" />
+                ) : (
+                  //  {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
+                  <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+                  // {/* shrink-0 is a tailwind class that prevents the icon from shrinking*/}
+                )}
+              </div>
+            );
+          })}
         </div>
 
         <Button variant="secondary" size="full">
